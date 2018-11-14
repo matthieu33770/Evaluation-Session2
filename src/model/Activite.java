a06b850f1ccecc2c6a17999679bb1856a88fac6d
Geraldine <geraldine.autriqueLP2@gmail.com>
2018-11-14 14:49:32 +0100

    activite

--- src/model/Activite.java
+++ src/model/Activite.java
@@ -1,5 +1,33 @@
 package model;
 
 public class Activite {
+	private int id;
+	private String activite;
+	
+	public Activite(int id, String activite) {
+		
+		this.id = id;
+		this.activite = activite;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getActivite() {
+		return activite;
+	}
+
+	public void setActivite(String activite) {
+		this.activite = activite;
+	}
+	
+	@Override
+	public String toString() {
+		return "Activite: numéro = " + id + "activite: "+ activite;
+				}
 }
