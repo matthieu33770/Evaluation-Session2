60df59bfdd5a3efd44210688e3f320f48e69b747
SIMFOR078 <matthielondeix@gmail.com>
2018-11-14 15:04:29 +0100

    Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,9 @@
 package main;
 
-import model.Activite;
-
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		
-=======
-		System.out.println();
->>>>>>> branch 'master' of https://github.com/matthieu33770/Evaluation-Session2.git
+
 
 	}
 
