2aee5c9335876472072c379b7fc26460f74621f5
SIMFOR078 <matthielondeix@gmail.com>
2018-11-14 15:42:04 +0100

    Main Apprenant

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,28 @@
 package main;
 
+import java.sql.SQLException;
+
+import model.Apprenant;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+		try {
+			// Les requêtes SELECT
+		
+				System.out.println("=================================================================");
+				System.out.println("\nListe des Apprenants avec getAllApprenant");
+				for (Apprenant apprenant : metier.Requetes.getAllApprenant()) {
+					System.out.println(apprenant + "\n");
+				}
+		} catch (ClassNotFoundException e) {
+			
+			e.printStackTrace();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
 
 	}
 
